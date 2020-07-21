48fc5b6896c67e01e333dae8ead537f562632844
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-07-21 16:08:08 +0300

    Complete app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+
+import LoadingBar from "react-redux-loading";
+import Nav from "./Nav";
+import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return (
-      <div>
-        Starter Code
-      </div>
-    )
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Nav />
+            {this.props.loading ? null : (
+              <div>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/tweet/:id" component={TweetPage} />
+                <Route path="/new" component={NewTweet} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </Router>
+    );
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
